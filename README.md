8e3a90e4797caabd41f76c54366878b58fa39581
mazz <me@mazz.tv>
2024-10-02 11:54:46 -0500

    Add files via upload
    - added helper function to target top-left layer for both commands
    - updated manifest
    - updated readme

--- README.md
+++ README.md
@@ -4,16 +4,17 @@ the sketch plugin that magically starts your page from (0,0)
 
 ## installation
 
-- [download](https://cdn.jsdelivr.net/gh/flavormingo/zerozero@master/zerozero.sketchplugin.zip) the zip
-- open the .zip
+- [download](../../releases/latest/download/zerozero.sketchplugin.zip) the zip
+- open the zip
 - double-click zerozero.sketchplugin
 
 ## usage
 
-`from top` - sets top-most layer's position to (0,0) and moves other layers accordingly
+`from left` - sets the left-most layer's position to (0,0) and moves other layers accordingly
 
-`from left` - you guessed it.. same thing, but targeting the left-most layer
+`from top` - you guessed it.. same thing, but targeting the top-most layer
 
+*if there are multiple target layers with the same x or y point, the top-left layer will be moved to (0,0)*
 *works on selections too* 😼
 
 ## credit
--- src/left.js
+++ src/left.js
@@ -1,55 +1,44 @@
 var sketch = require('sketch')
 
+function findTarget(layers, primaryAxis, secondaryAxis) {
+  let minPrimary = Math.min(...layers.map(layer => layer.frame[primaryAxis]))
+  const primaryLayers = layers.filter(layer => layer.frame[primaryAxis] === minPrimary)
+
+  let minSecondary = Math.min(...primaryLayers.map(layer => layer.frame[secondaryAxis]))
+  return primaryLayers.find(layer => layer.frame[secondaryAxis] === minSecondary)
+}
+
 export default function() {
 
   const document = sketch.getSelectedDocument()
   const selection = document.selectedLayers
 
-  if (selection.length === 0) {
-    const page = document.selectedPage
-    const layers = page.layers
-
-    if (layers.length === 0) {
-      sketch.UI.message('zerozero » no layers on this page 😿')
-      return
-    }
-
-    let left = layers[0]
-    layers.forEach(layer => {
-      if (layer.frame.x < left.frame.x) {
-        left = layer
-      }
-    })
-
-    const deltaX = -left.frame.x
-    const deltaY = -left.frame.y
-
-    layers.forEach(layer => {
-      layer.frame.x += deltaX
-      layer.frame.y += deltaY
-    })
-  } else if (selection.length === 1) {
+  if (selection.length === 1) {
     const layer = selection.layers[0]
     layer.frame.x = 0
     layer.frame.y = 0
-  } else {
-    const layers = selection.layers
-
-    let left = layers[0]
-    layers.forEach(layer => {
-      if (layer.frame.x < left.frame.x) {
-        left = layer
-      }
-    })
-
-    const deltaX = -left.frame.x
-    const deltaY = -left.frame.y
-
-    layers.forEach(layer => {
-      layer.frame.x += deltaX
-      layer.frame.y += deltaY
-    })
+    sketch.UI.message('zerozero » layer moved to (0,0) 😼')
+    return
   }
+
+  const layers = selection.length === 0
+    ? document.selectedPage.layers
+    : selection.layers
+
+  if (layers.length === 0) {
+    sketch.UI.message('zerozero » no layers on this page 😿')
+    return
+  }
+
+  const target = findTarget(layers, 'x', 'y')
+
+  const deltaX = -target.frame.x
+  const deltaY = -target.frame.y
+
+  layers.forEach(layer => {
+    layer.frame.x += deltaX
+    layer.frame.y += deltaY
+  })
 
   sketch.UI.message('zerozero » organized from left-most layer 😸')
 }
